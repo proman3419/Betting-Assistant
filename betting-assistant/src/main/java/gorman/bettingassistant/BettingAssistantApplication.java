4157fed6e5027e21bc963bfd67c7b8326ca4a1cf
proman3419 <proman7268@gmail.com>
2022-12-04 19:14:53 +0100

    Project backbone

---
+++ betting-assistant/src/main/java/gorman/bettingassistant/BettingAssistantApplication.java
@@ -0,0 +1,13 @@
+package gorman.bettingassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettingAssistantApplication.class, args);
+	}
+
+}
